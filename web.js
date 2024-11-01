14533c09996aee3439bf0993b291cfd81b189925
victoriaviv <dufatanyevictoria2@gmail.com>
2024-11-01 23:01:42 +0200

    add web.js

---
+++ web.js
@@ -0,0 +1,9 @@
+let myService1={
+    icon:"ristlartphone-line",
+    title:"web design",
+    description:"we are dedicted to providing high-quality web design services that are tailored to meet the specific needs of our clients."
+}
+
+console.log(myService1);
+document.getElementById("myService").innerHTML =
+"myService1";
